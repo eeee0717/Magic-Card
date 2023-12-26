4245ee95799aaa02a19d52a24cdf85754634ad0d
eeee0717 <chentao020717Work@outlook.com>
2023-12-26 16:58:16 +0800

    Initial Commit

---
+++ Magic-Card/Magic_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_CardApp.swift
+//  Magic-Card
+//
+//  Created by eeee on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Magic_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
